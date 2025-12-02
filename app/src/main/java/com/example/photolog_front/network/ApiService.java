a3afd965312af192f4b796955c86986a806cfdf3
decembix <xyzysh@gmail.com>
2025-12-02 17:16:39 +0900

    와 로그인 된다!!!!!!!!!!

--- app/src/main/java/com/example/photolog_front/network/ApiService.java
+++ app/src/main/java/com/example/photolog_front/network/ApiService.java
@@ -1,5 +1,7 @@
 package com.example.photolog_front.network;
 
+import com.example.photolog_front.model.LoginRequest;
+import com.example.photolog_front.model.LoginResponse;
 import com.example.photolog_front.model.SignupResponse;
 import com.google.gson.JsonObject;
 import com.example.photolog_front.model.SignupRequest;
@@ -12,6 +14,8 @@ import retrofit2.http.POST;
 public interface ApiService {
     @POST("/signup")
     Call<SignupResponse> signup(@Body SignupRequest request);
+    @POST("/login")
+    Call<LoginResponse> login(@Body LoginRequest request);
     @POST("/api/chat")
     Call<ResponseBody> sendUserMessage(@Body JsonObject body);
 
